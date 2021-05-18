f2c22c2d203238941c49aac783648a6d34fe8ba0
peter <peetmhlanga@gmail.com>
2021-05-18 19:50:41 +0200

    completed read in of input data

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,29 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'{name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+def read_data():
+    lines = []
+    with open('input.txt') as f:
+        lines = f.readlines()
+    return lines
+
+
+# define main method
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    print_hi('K-means')
+    the_lines = read_data()
+
+    count = 0
+    for line in the_lines:
+        count += 1
+        print(f'line {count}: {line}')
+
+
+
+
+
+
